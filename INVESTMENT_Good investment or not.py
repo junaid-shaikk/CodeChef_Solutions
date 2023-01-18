df82a11ca8c92a4db6b2e49aff6f4da11e42aa37
Junaid Shaik <70793625+junaid-shaikk@users.noreply.github.com>
2023-01-18 23:12:36 +0530

    Create INVESTMENT_Good investment or not.py

---
+++ INVESTMENT_Good investment or not.py
@@ -0,0 +1,20 @@
+'''
+Problem
+Chef has invested his money at an interest rate of 
+�
+X percent per annum while the current inflation rate is 
+�
+Y percent per annum.
+
+An investment is called good if and only if the interest rate of the investment is at least twice of the inflation rate.
+Determine whether the investment made by Chef is good or not.
+'''
+
+#code
+
+for i in range(int(input())):
+    a,b=map(int,input().split())
+    if (a/2)>=b:
+        print('YES')
+    else:
+        print('NO')
